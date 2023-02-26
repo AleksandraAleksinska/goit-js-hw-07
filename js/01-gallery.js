b4c25607926d2261e187f22046d4b5df84d46d6b
AleksandraAleksinska <aleksandra.aleksinska@gmail.com>
2023-02-26 18:18:04 +0100

    links update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,4 +3,32 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+const galleryElements = galleryItems.map((item) => 
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </div>`
+    )
+    .join("");
+
+gallery.insertAdjacentHTML('beforeend', galleryElements);
+
+const biggerImg = (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+      }
+        const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600">`)
+
+        instance.show()       
+
+    };
+    gallery.addEventListener('click', biggerImg);
